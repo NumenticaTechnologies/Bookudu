537dabca294f1872cafbb56a6ef659e54c5d2db4
NumenticaTechnologies <58783954+NumenticaTechnologies@users.noreply.github.com>
2019-12-11 22:34:26 +0530

    Add files via upload

---
+++ src/Pages/Home_page.jsx
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from 'react'
+
+export default class Home_page extends Component {
+    render() {
+        return (
+            <Fragment>
+                <p>Bookudu Application</p>
+            </Fragment>
+        )
+    }
+}
